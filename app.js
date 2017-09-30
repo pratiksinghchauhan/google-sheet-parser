450a66508b75f45149d9564e3a3855bb40bb77b0
pratiksinghchauhan <pratiksingh773@gmail.com>
2017-10-01 00:23:26 +0530

    set auth and read a specific rowo

--- app.js
+++ app.js
@@ -1,50 +1,113 @@
 const Hapi = require('hapi');
 var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
 
 
 const server = new Hapi.Server();
 server.connection({
     host: 'localhost',
-    port: 8000
+    port: 7000
 });
 
-/*
-server.route({
-    method: 'GET',
-    path:'/', 
-    handler: function (request, reply) {
 
-        var doc = new GoogleSpreadsheet('1mEtpA4f4bpTG20b_N1vEbl4SI4zHF_eJVopwGEQa6t4');
-        var doc_rows;
-        
-        doc.getRows(1,{}, function(err,rows){
-            
-            if(rows){
-                doc_rows=rows;
-                console.log(rows);
-                reply({ rows : doc_rows }).code(200)
 
-            }
-            else{
-                console.log(err);
-                reply().code(400)
-            }
-            
-        });
-    }
-});
-*/
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
 
 var returnrows = function () {
-    console.log("spreadsheets");
-    var doc = new GoogleSpreadsheet('Add a publick google sheet id here');
-    doc.getRows(1, {}, function (err, rows) {
-        if (rows) {
-            console.log(rows);
-        } else {
-            console.log(err)
+
+    var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+    var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+        process.env.USERPROFILE) + '/.credentials/';
+    var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+    // Load client secrets from a local file.
+    fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+        if (err) {
+            console.log('Error loading client secret file: ' + err);
+            return;
         }
+        // Authorize a client with the loaded credentials, then call the
+        // Google Sheets API.
+        authorize(JSON.parse(content), listMajors);
     });
+
+
+    function authorize(credentials, callback) {
+        var clientSecret = credentials.installed.client_secret;
+        var clientId = credentials.installed.client_id;
+        var redirectUrl = credentials.installed.redirect_uris[0];
+        var auth = new googleAuth();
+        var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+        // Check if we have previously stored a token.
+        fs.readFile(TOKEN_PATH, function (err, token) {
+            if (err) {
+                getNewToken(oauth2Client, callback);
+            } else {
+                oauth2Client.credentials = JSON.parse(token);
+                callback(oauth2Client);
+            }
+        });
+    }
+
+
+    function getNewToken(oauth2Client, callback) {
+        var authUrl = oauth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: SCOPES
+        });
+        console.log('Authorize this app by visiting this url: ', authUrl);
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('Enter the code from that page here: ', function (code) {
+            rl.close();
+            oauth2Client.getToken(code, function (err, token) {
+                if (err) {
+                    console.log('Error while trying to retrieve access token', err);
+                    return;
+                }
+                oauth2Client.credentials = token;
+                storeToken(token);
+                callback(oauth2Client);
+            });
+        });
+    }
+
+
+    function storeToken(token) {
+        try {
+            fs.mkdirSync(TOKEN_DIR);
+        } catch (err) {
+            if (err.code != 'EEXIST') {
+                throw err;
+            }
+        }
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+        console.log('Token stored to ' + TOKEN_PATH);
+    }
+
+
+    function listMajors(auth) {
+        var sheets = google.sheets('v4');
+        sheets.spreadsheets.values.get({
+            auth: auth,
+            spreadsheetId: '1VWv17f9QtUBCpoGFhLiPsYWYUlM26JyAXa9mw3ff8XE',
+            range: 'Sheet1!A1:A',
+        }, function (err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                return;
+            }
+            var rows = response.values;
+            console.log('rows: ' + rows.length);
+        });
+    }
+
 }
 
 var callback = function () {
@@ -57,7 +120,7 @@ server.register({
         jobs: [{
             name: "Call api two times a day",
             enabled: true,
-            immediate: false,
+            immediate: true,
             schedule: "every 12 h",
             execute: returnrows,
             environments: ['development', 'staging']
